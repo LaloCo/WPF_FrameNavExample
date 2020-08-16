e7ae74593262ca05cf71d6fc70e591bf00bcdb8c
Eduardo Rosas <lalorosas@outlook.com>
2020-08-16 11:07:12 -0500

    Initial commit

---
+++ WpfApp2/MainVM.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfApp2
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string path;
+
+        public string Path
+        {
+            get { return path; }
+            set 
+            { 
+                path = value;
+                OnPropertyChanged("Path");
+            }
+        }
+
+        private ICommand _navigateCommand;
+
+        public ICommand NavigateCommand
+        {
+            get 
+            { 
+                return _navigateCommand ?? (_navigateCommand = new CommandHandler(() => Navigate(), () => true));
+            }
+        }
+
+
+        public MainVM()
+        {
+            Path = "PageONe.xaml";
+        }
+
+        private void Navigate()
+        {
+            Path = "PageTwo.xaml";
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class CommandHandler : ICommand
+    {
+        private Action _action;
+        private Func<bool> _canExecute;
+
+        /// <summary>
+        /// Creates instance of the command handler
+        /// </summary>
+        /// <param name="action">Action to be executed by the command</param>
+        /// <param name="canExecute">A bolean property to containing current permissions to execute the command</param>
+        public CommandHandler(Action action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// Wires CanExecuteChanged event 
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// Forcess checking if execute is allowed
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute.Invoke();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
